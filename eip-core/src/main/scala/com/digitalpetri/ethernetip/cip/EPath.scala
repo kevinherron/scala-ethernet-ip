c121cbc0076254b69359cefd552de4438877ba9d
Kevin Herron <kevinherron@gmail.com>
2014-04-11 18:02:03 -0700

    Make EPath placeholder a case class for now...

--- eip-core/src/main/scala/com/digitalpetri/ethernetip/cip/EPath.scala
+++ eip-core/src/main/scala/com/digitalpetri/ethernetip/cip/EPath.scala
@@ -2,7 +2,7 @@ package com.digitalpetri.ethernetip.cip
 
 import io.netty.buffer.ByteBuf
 
-trait EPath // TODO
+case class EPath() // TODO
 
 object EPath {
   def encode(path: EPath, buffer: ByteBuf) {
