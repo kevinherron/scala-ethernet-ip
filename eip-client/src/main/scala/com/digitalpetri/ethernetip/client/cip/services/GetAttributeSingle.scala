a7dbb72fc0b10d95de2954483a2b5f387e3e25aa
Kevin Herron <kevinherron@gmail.com>
2014-04-16 21:02:13 -0700

    GetAttributeSingle

---
+++ eip-client/src/main/scala/com/digitalpetri/ethernetip/client/cip/services/GetAttributeSingle.scala
@@ -0,0 +1,48 @@
+/*
+ * EtherNet/IP
+ * Copyright (C) 2014 Kevin Herron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.digitalpetri.ethernetip.client.cip.services
+
+import com.digitalpetri.ethernetip.cip.CipServiceCodes
+import com.digitalpetri.ethernetip.cip.epath.PaddedEPath
+import com.digitalpetri.ethernetip.cip.services.GetAttributeSingleService.GetAttributeSingleResponse
+import com.digitalpetri.ethernetip.cip.structs.MessageRouterRequest
+import io.netty.buffer.{Unpooled, ByteBuf}
+import scala.util.Try
+
+class GetAttributeSingle(requestPath: PaddedEPath) extends AbstractInvokableService[GetAttributeSingleResponse] {
+
+  def getRequestData: ByteBuf = {
+    val routerRequest = MessageRouterRequest(
+      serviceCode = CipServiceCodes.GetAttributeSingle,
+      requestPath = requestPath,
+      requestData = Unpooled.EMPTY_BUFFER)
+
+    MessageRouterRequest.encode(routerRequest)
+  }
+
+  /**
+   * Decode `responseData` and return a response.
+   * @param responseData the [[ByteBuf]] containing the response data.
+   * @return a decoded response.
+   */
+  override def decodeResponse(responseData: ByteBuf): Try[GetAttributeSingleResponse] = {
+    GetAttributeSingleResponse.decode(responseData)
+  }
+
+}
