77ad9e8b6c1384a8e805ec9fa554f11b7c5529e8
Kevin Herron <kevinherron@gmail.com>
2014-05-06 06:26:03 -0700

    Limit concurrent unconnected requests

--- enip-client/src/main/scala/com/digitalpetri/ethernetip/client/cip/CipServiceInvoker.scala
+++ enip-client/src/main/scala/com/digitalpetri/ethernetip/client/cip/CipServiceInvoker.scala
@@ -3,6 +3,7 @@ package com.digitalpetri.ethernetip.client.cip
 import com.digitalpetri.ethernetip.cip.services.MultipleServicePacket.MultipleServicePacketRequest
 import com.digitalpetri.ethernetip.cip.services.UnconnectedSend.UnconnectedSendRequest
 import com.digitalpetri.ethernetip.client.cip.services.{InvokableService, MultipleServicePacketService, UnconnectedSendService}
+import com.digitalpetri.ethernetip.client.util.AsyncSemaphore
 import io.netty.buffer.ByteBuf
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
@@ -12,12 +13,14 @@ trait CipServiceInvoker extends CipConnectionManager {
 
   private implicit val executionContext = config.executionContext
 
+  private val unconnectedSemaphore = new AsyncSemaphore(config.concurrency)
+
   def invokeService[T](service: InvokableService[T], connected: Boolean = false): Future[T] = {
     if (connected) {
       /*
        * Connected Explicit Message
        */
-      reserveConnection().onComplete {
+      acquireConnection().onComplete {
         case Success(connection) =>
           def _send(requestData: ByteBuf, o2tConnectionId: Int) {
             sendConnectedData(requestData, connection.o2tConnectionId).onComplete {
@@ -28,6 +31,7 @@ trait CipServiceInvoker extends CipConnectionManager {
                 }
 
               case Failure(ex) =>
+                releaseConnection(connection)
                 service.setResponseFailure(ex)
             }
           }
@@ -41,36 +45,46 @@ trait CipServiceInvoker extends CipConnectionManager {
       /*
        * Unconnected Explicit Message
        */
-      def _send(requestData: ByteBuf) {
-        val request = UnconnectedSendRequest(
-          timeout         = config.timeout,
-          embeddedRequest = requestData,
-          connectionPath  = config.connectionPath)
+      unconnectedSemaphore.acquire().onComplete {
+        case Success(permit) =>
+          def _send(requestData: ByteBuf) {
+            val request = UnconnectedSendRequest(
+              timeout         = config.requestTimeout,
+              embeddedRequest = requestData,
+              connectionPath  = config.connectionPath)
 
-        val unconnectedService = new UnconnectedSendService(request)
+            val unconnectedService = new UnconnectedSendService(request)
 
-        unconnectedService.response.onComplete {
-          case Success(responseData) =>
-            service.setResponseData(responseData).map(_send)
+            unconnectedService.response.onComplete {
+              case Success(responseData) =>
+                service.setResponseData(responseData) match {
+                  case Some(d) => _send(d)
+                  case None => permit.release()
+                }
 
-          case Failure(ex) =>
-            service.setResponseFailure(ex)
-        }
+              case Failure(ex) =>
+                permit.release()
+                service.setResponseFailure(ex)
+            }
 
-        sendUnconnectedData(unconnectedService.getRequestData).onComplete {
-          case Success(responseData) => unconnectedService.setResponseData(responseData)
-          case Failure(ex) => unconnectedService.setResponseFailure(ex)
-        }
+            sendUnconnectedData(unconnectedService.getRequestData).onComplete {
+              case Success(responseData) => unconnectedService.setResponseData(responseData)
+              case Failure(ex) => unconnectedService.setResponseFailure(ex)
+            }
+          }
+
+          _send(service.getRequestData)
+
+        case Failure(ex) => service.setResponseFailure(ex)
       }
 
-      _send(service.getRequestData)
     }
 
     service.response
   }
 
   def invokeMultiple(services: Seq[InvokableService[_]], connected: Boolean): Future[_] = {
-    def invoke(services: Seq[InvokableService[_]], requests: Seq[ByteBuf]): Future[_] = {
+    def _invoke(services: Seq[InvokableService[_]], requests: Seq[ByteBuf]): Future[_] = {
       assert(services.size == requests.size)
 
       if (services.isEmpty || requests.isEmpty) return Future.successful(Unit)
@@ -79,6 +93,8 @@ trait CipServiceInvoker extends CipConnectionManager {
 
       service.response.onComplete {
         case Success(response) =>
+          assert(services.size == response.serviceResponses.size)
+
           val incomplete = services.zip(response.serviceResponses).flatMap {
             case (s, d) => s.setResponseData(d).map(next => (s, next))
           }
@@ -91,7 +107,7 @@ trait CipServiceInvoker extends CipConnectionManager {
             val nextServices = incomplete.map(_._1)
             val nextRequests = incomplete.map(_._2)
 
-            invoke(nextServices, nextRequests)
+            _invoke(nextServices, nextRequests)
           }
 
         case Failure(ex) => services.foreach(_.setResponseFailure(ex))
@@ -100,7 +116,7 @@ trait CipServiceInvoker extends CipConnectionManager {
       invokeService(service, connected)
     }
 
-    invoke(services, services.map(_.getRequestData))
+    _invoke(services, services.map(_.getRequestData))
   }
 
 }
---
+++ enip-client/src/main/scala/com/digitalpetri/ethernetip/client/util/AsyncSemaphore.scala
@@ -0,0 +1,81 @@
+package com.digitalpetri.ethernetip.client.util
+
+import java.util
+import java.util.concurrent.RejectedExecutionException
+import scala.concurrent.{Future, Promise}
+
+/**
+ * An AsyncSemaphore is a traditional semaphore but with asynchronous
+ * execution. Grabbing a permit returns a Future[Permit].
+ */
+class AsyncSemaphore protected (initialPermits: Int, maxWaiters: Option[Int]) {
+
+  import AsyncSemaphore._
+
+  def this(initialPermits: Int = 0) = this(initialPermits, None)
+  def this(initialPermits: Int, maxWaiters: Int) = this(initialPermits, Some(maxWaiters))
+  require(maxWaiters.getOrElse(0) >= 0)
+
+  private val waitQueue = new util.ArrayDeque[Promise[Permit]]
+  private var availablePermits = initialPermits
+
+  private class SemaphorePermit extends Permit {
+    /**
+     * Indicate that you are done with your Permit.
+     */
+    override def release() {
+      val p: Promise[Permit] = AsyncSemaphore.this.synchronized {
+        val next = waitQueue.pollFirst()
+        if (next == null) availablePermits += 1
+        next
+      }
+
+      if (p != null) p.success(new SemaphorePermit)
+    }
+  }
+
+  def numWaiters: Int = synchronized(waitQueue.size)
+  def numPermitsAvailable: Int = synchronized(availablePermits)
+
+  /**
+   * Acquire a Permit, asynchronously. Be sure to permit.release() in a 'finally'
+   * block of your onSuccess() callback.
+   *
+   * Interrupting this future is only advisory, and will not release the permit
+   * if the future has already been satisfied.
+   *
+   * @return a Future[Permit] when the Future is satisfied, computation can proceed,
+   * or a Future.Exception[RejectedExecutionException] if the configured maximum number of waitQueue
+   * would be exceeded.
+   */
+  def acquire(): Future[Permit] = {
+    synchronized {
+      if (availablePermits > 0) {
+        availablePermits -= 1
+        Future.successful(new SemaphorePermit)
+      } else {
+        maxWaiters match {
+          case Some(max) if waitQueue.size >= max =>
+            MaxWaitersExceededException
+
+          case _ =>
+            val promise = Promise[Permit]()
+            waitQueue.addLast(promise)
+            promise.future
+        }
+      }
+    }
+  }
+
+}
+
+object AsyncSemaphore {
+  private val MaxWaitersExceededException =
+    Future.failed(new RejectedExecutionException("max waiters exceeded"))
+
+  trait Permit {
+    def release()
+  }
+}
+
+
